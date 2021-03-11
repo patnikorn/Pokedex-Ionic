77c25a0b60e5b00dc5e6a32fa2f211f9518e5284
patnikorn <patnikorn.ta.61@ubu.ac.th>
2021-03-11 23:03:48 +0700

    first commit

---
+++ src/app/details/details.page.ts
@@ -0,0 +1,28 @@
+import { PokemonService } from './../services/pokemon.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+ 
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.page.html',
+  styleUrls: ['./details.page.scss'],
+})
+export class DetailsPage implements OnInit {
+  details: any;
+ 
+  slideOpts = {
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false
+    }
+  };
+ 
+  constructor(private pokeService: PokemonService, private route: ActivatedRoute) { }
+ 
+  ngOnInit() {
+    let index = this.route.snapshot.paramMap.get('index');
+    this.pokeService.getPokeDetails(index).subscribe(details => {
+      this.details = details;
+    });
+  }
+}
